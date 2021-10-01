d6ff676971a925aeb2b2a3fc8289f4a3a5f8b37f
Andrew <andrey.yokhna@yandex.ru>
2021-10-01 11:53:11 +0700

    Avatar

--- src/Header/img/Dialog.jsx
+++ src/Header/img/Dialog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import p from './Dialog.module.css';
+import p from '../../Dialog/Dialog.module.css';
 import {BrowserRouter, Route} from "react-router-dom";
 
 const DialogItem = (props) =>
--- src/Profile/Profile_data/jsx/Header.jsx
+++ src/Profile/Profile_data/jsx/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ps from '../../Profile.module.css';
 import shapka from '../img/Shapka.png';
+import shapka_2 from "../../../Header/img/Head_shapka.jpeg";
+import avatar from "./Avatar";
 
 const Head = () => {
     return <div className={ps.header_1}>
             <img src={shapka} alt='shapka' />
+        <img src={avatar} alt='avatar'/>
         </div>
     }
     export default Head;
