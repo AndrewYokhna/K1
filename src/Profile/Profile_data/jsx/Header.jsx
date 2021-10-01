2c98242b7d9a3ffb1b55d3585f140f9ff59995d7
Andrew <andrey.yokhna@yandex.ru>
2021-10-01 12:40:55 +0700

    Back color

--- src/Profile/Profile_data/jsx/Header.jsx
+++ src/Profile/Profile_data/jsx/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ps from '../../Profile.module.css';
 import shapka from '../img/Shapka.png';
-import shapka_2 from "../../../Header/img/Head_shapka.jpeg";
-import avatar from "./Avatar";
+
+
 
 const Head = () => {
     return <div className={ps.header_1}>
