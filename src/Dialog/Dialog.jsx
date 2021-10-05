244d8dfa9310cf0e02bf4f388ef037a76a71c946
Андрей <andrey.yokhna@yandex.ru>
2021-10-05 20:57:10 +0700

    +line

--- src/Dialog/Dialog.jsx
+++ src/Dialog/Dialog.jsx
@@ -2,42 +2,52 @@ import React from 'react';
 import p from './Dialog.module.css';
 import {NavLink} from "react-router-dom";
 
+const Dialog_item = (props) => {
+    return <div className={p.dialog}>
+        <NavLink to={'dialog/' + props.id}>{props.name}</NavLink>
+    </div>
+}
 
-const Dialog = () => {
+const Dialog = (props) => {
     return <div className={p.dialog_grid}>
-        <div className={p.dialog_background + ' ' + p.dialogsItem + ' ' + p.dialog}>
-            <div>
-                <NavLink to='/dialog/1'> Andrey </NavLink>
-            </div>
-            <div>
-                <NavLink to='/dialog/2'> Dima </NavLink>
-            </div>
-            <div>
-                <NavLink to='/dialog/3'> Viktor </NavLink>
-            </div>
-            <div>
-                <NavLink to='/dialog/4'> Natali </NavLink>
-            </div>
-        </div>
-        <div className={p.messages_background}>
-            <div className={p.message + ' ' + p.messages}>
-                <div>
-                    Hello
-                </div>
-                <div>
-                    Hello
-                </div>
-                <div>
-                    Hello
-                </div>
-                <div>
-                    Hello
-                </div>
-            </div>
-        </div>
+        <div className={p.dialog_background + ' ' + p.dialogsItem}>
+           <div>
+            <Dialog_item name='Andrey' id='1'/>
 
+        </div>
+        <div>
+            <Dialog_item name='Viktor' id='2'/>
+
+        </div>
+        <div>
+            <Dialog_item name='Natali' id='3'/>
+
+        </div>
+        <div>
+            <Dialog_item name='Joe' id='4'/>
+
+        </div>
 
     </div>
+    <div className={p.messages_background}>
+        <div className={p.message + ' ' + p.messages}>
+            <div>
+                Hello
+            </div>
+            <div>
+                Hello
+            </div>
+            <div>
+                Hello
+            </div>
+            <div>
+                Hello
+            </div>
+        </div>
+    </div>
+
+
+</div>
 }
 
 export default Dialog;
