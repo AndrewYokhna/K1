f3426d6e19322b074dbd59a370db96e3ad9da47d
Андрей <andrey.yokhna@yandex.ru>
2021-10-17 18:58:59 +0700

    _

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import p from './Nav.module.css';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return <div className={p.nav}>
