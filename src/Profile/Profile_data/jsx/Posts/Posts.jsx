2c30ff98c987120ce0938906241b7f4990322440
Андрей <andrey.yokhna@yandex.ru>
2022-01-29 16:50:36 +0700

    1

--- src/Profile/Profile_data/jsx/Posts/Posts.jsx
+++ src/Profile/Profile_data/jsx/Posts/Posts.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-<<<<<<< HEAD:src/Profile/Profile_data/jsx/Posts/Posts.jsx
+// import Posts from "./src/Profile/Profile_data/jsx/Posts/Posts.jsx"
 import p from '../../css/Posts.module.css';
-=======
 import p from '../css/Posts.module.css';
 import like from './../img/like.png';
->>>>>>> 7bfbbfb4555b13cce73c104dc85962f4ce06b2ef:src/Profile/Profile_data/jsx/Posts.jsx
 
 
 const Posts_Item = (props) => {
