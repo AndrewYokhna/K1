dbd38b9c941438069e1064ca449a8a21e61efafa
AndrewYokhna <andrey.yokhna@Yandex.ru>
2022-02-05 20:18:10 +0700

    1

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import p from './Head.module.css';
 import shapka_2 from './img/Head_shapka.jpeg';
+import logo from './img/logo.png';
 
 const Header = (props) => {
     return <header className={p.header}>
-        <img src={shapka_2} alt='shapka_2'/>
-
-
-</header>
+        <div>
+            <img className={p.img_head} src={shapka_2} alt='shapka_2' />
+            <img className={p.img_logo} src={logo} alt='logo' />
+        </div>
+    </header>
 }
 
 export default Header;
